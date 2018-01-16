f6438b1f7208b68e3ce4ecf04c29c86a6c18566e
pmall <pierre.mallinjoud@gmail.com>
2018-01-16 11:50:38 +0100

    add a router middleware

---
+++ spec/RouterMiddleware.spec.php
@@ -0,0 +1,75 @@
+<?php
+
+use function Eloquent\Phony\Kahlan\mock;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+
+use Ellipse\Router\RouterMiddleware;
+use Ellipse\Router\RouterRequestHandler;
+use Ellipse\Router\Exceptions\NotFoundException;
+
+describe('RouterMiddleware', function () {
+
+    beforeEach(function () {
+
+        $this->router = mock(RouterRequestHandler::class);
+
+        $this->middleware = new RouterMiddleware($this->router->get());
+
+    });
+
+    it('should implement MiddlewareInterface', function () {
+
+        expect($this->middleware)->toBeAnInstanceOf(MiddlewareInterface::class);
+
+    });
+
+    describe('->process()', function () {
+
+        beforeEach(function () {
+
+            $this->request = mock(ServerRequestInterface::class);
+            $this->response = mock(ResponseInterface::class);
+            $this->handler = mock(RequestHandlerInterface::class);
+
+        });
+
+        context('when the router does not throw a NotFoundException', function () {
+
+            it('should proxy the router ->handle() method', function () {
+
+                $this->router->handle->with($this->request)->returns($this->response);
+
+                $test = $this->middleware->process($this->request->get(), $this->handler->get());
+
+                expect($test)->toBe($this->response->get());
+
+            });
+
+        });
+
+        context('when the router throws a NotFoundException', function () {
+
+            it('should proxy the given handler ->handle() method', function () {
+
+                $exception = mock(NotFoundException::class)->get();
+
+                $this->router->handle->with($this->request)->throws($exception);
+
+                $this->handler->handle->with($this->request)->returns($this->response);
+
+                $test = $this->middleware->process($this->request->get(), $this->handler->get());
+
+                expect($test)->toBe($this->response->get());
+
+            });
+
+        });
+
+    });
+
+});
---
+++ src/RouterMiddleware.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace Ellipse\Router;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+
+use Ellipse\Router\Exceptions\NotFoundException;
+
+class RouterMiddleware implements MiddlewareInterface
+{
+    /**
+     * The router request handler.
+     *
+     * @var \Ellipse\Router\RouterRequestHandler
+     */
+    private $router;
+
+    /**
+     * Set up a router middleware with the given router request handler.
+     *
+     * @param \Ellipse\Router\RouterRequestHandler $router
+     */
+    public function __construct(RouterRequestHandler $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Try to proxy the router ->handle() method. Proxy the given request
+     * handler when the router throws a not found exception.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface      $request
+     * @param \Interop\Http\Server\RequestHandlerInterface  $handler
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+
+            return $this->router->handle($request);
+
+        }
+
+        catch (NotFoundException $e) {
+
+            return $handler->handle($request);
+
+        }
+    }
+}
